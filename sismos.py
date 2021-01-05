fbc7216b14887d96d39e3f6edb44dd9d0a5e6e29
amcabezas <alexis.cabezas.navarrete@gmail.com>
2021-01-04 23:15:28 -0300

    Update to python3.9

--- sismos.py
+++ sismos.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python
-# -*- coding: latin-1 -*-
+#!/usr/bin/python3
 from bs4 import BeautifulSoup
 import requests
 url = 'http://www.sismologia.cl/links/tabla.html'
 web = requests.get(url)
 html = BeautifulSoup(web.text,"lxml")
 tabla = html.find("table")
-print 'Información de los últimos sismos en Chile.'
+print("Información de los últimos sismos en Chile.")
 for row in tabla.findAll("tr", limit=4):
     if row.find_all('td') == []:
         pass
@@ -15,10 +14,10 @@ for row in tabla.findAll("tr", limit=4):
         b = row.findAll('td')[1:2]
         c = row.findAll('td')[2:3]
         for fec in a:
-            print 'Fecha:',fec.getText()
+            print("Fecha:",fec.getText())
         for lug in b:
-            print 'Lugar',lug.getText().encode('latin-1')
+            print("Lugar",lug.getText().encode("latin-1").decode("utf-8"))
         for mag in c:
-            print 'Magnitud',mag.getText()
-            print '-------------------------'
-print 'Fuente:Centro Sismológico Nacional, Universidad de Chile. www.sismologia.cl'
+            print("Magnitud",mag.getText())
+            print('-------------------------')
+print("Fuente:Centro Sismológico Nacional, Universidad de Chile. www.sismologia.cl")
